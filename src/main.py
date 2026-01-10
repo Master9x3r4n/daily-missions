09050e9b1e7b2bb1bc5cc38e0383d1d11c02ac61
Stephen <stephen.jszs.gplay@gmail.com>
2026-01-10 22:17:35 +0800

    feat: implemented cards system and linked mission_manager to gui

--- src/main.py
+++ src/main.py
@@ -9,6 +9,7 @@
 """
 
 import pygame as pg
+import math
 from mission_manager import MissionManager
 from surface_manager import SurfaceManager
 
@@ -19,9 +20,22 @@ if __name__ == "__main__":
     mm = MissionManager()
     screen = SurfaceManager()
 
+    #load mission data
+    mm.update_missions(data_path)
+
+    # initialize variables
     program_running = True
+    CARDS = len(mm.missions)
+    MAX_CARDS = 4
+    card_count = CARDS % MAX_CARDS
+    card_index = -1
+    page = 1
+
     while program_running:
-        # Event handling
+        ####################
+        #  EVENT HANDLING  #
+        ####################
+        mm.update_missions(data_path)
         for event in pg.event.get():
             # Check close program
             if event.type == pg.QUIT:
@@ -31,20 +45,44 @@ if __name__ == "__main__":
             #Menu events
             if screen.next_button_pressed(event):
                 print("next")
+                page = min(math.ceil(CARDS / MAX_CARDS), page + 1)
+
+            if screen.back_button_pressed(event):
+                print("back")
+                page = max(1, page - 1)
 
             if screen.reset_button_pressed(event):
                 print("reset")
+                page = 1
 
             if screen.add_button_pressed(event):
                 print("add")
 
-            if screen.back_button_pressed(event):
-                print("back")
+            index_button = screen.index_button_pressed(event)
+            if index_button:
+                card_index = 4 * (page - 1) + index_button - 1
+                print(card_index)
 
-        #Display events
-        screen.draw_menu()
+        ####################
+        #  LOGIC HANDLING  #
+        ####################
+        if card_index > -1:
+            mm.missions[card_index].is_done = True
 
+        #reset variables
+        card_count = min(max(CARDS - (page - 1) * 4, 0), 4)
+        card_index = -1
 
+        #update file
+        mm.update_file(data_path)
+
+        ####################
+        # DISPLAY HANDLING #
+        ####################
+        # Draw Menu
+        screen.draw_menu(card_count, page)
+
+        # Update display
         pg.display.update()
 
     pg.quit()
--- src/surface_manager.py
+++ src/surface_manager.py
@@ -13,10 +13,15 @@ class SurfaceManager:
         self.SURFACE = pg.display.set_mode((self.WIDTH, self.HEIGHT))
         pg.display.set_caption("Daily Missions")
 
+        # Initialize buttons
         self.button_back: Button = None
         self.button_add: Button = None
         self.button_reset: Button = None
         self.button_next: Button = None
+        self.button_index1: Button = None
+        self.button_index2: Button = None
+        self.button_index3: Button = None
+        self.button_index4: Button = None
         self.init_buttons()
 
     ##################
@@ -39,28 +44,44 @@ class SurfaceManager:
         return Button(x, y, text = text, font_size = font_size, color= button_color, text_color= font_color, surface=self.SURFACE)
 
     ##################
-    #     PANELS     #
-    ##################
-    def draw_background(self):
-        # bg color
-        self.draw_rect("#D9D9D9", (0, 0, self.WIDTH, self.HEIGHT))
-
-    def draw_progress_bar(self):
-        #progress bar
-        self.draw_rect("#FFFFFF", (136 * self.WR, 58 * self.HR, 1760 * self.WR, 180 * self.HR), 32)
-        self.draw_circle("#7E6D4B", 136 * self.WR, 148 * self.HR, 65)
-
-    def draw_mission_panel(self):
-        self.draw_rect("#FF0000", (0, 264 * self.HR, self.WIDTH, 680 * self.HR))
-
-    ##################
     #     BUTTON     #
     ##################
-    def draw_buttons(self):
+    def init_buttons(self):
+        # menu buttons
+        baseline = self.HR * (264 + 680)
+        init_x = self.WIDTH / 2
+        button_color = "#EDEDED"
+        font_color = "#FFB435"
+
+        self.button_back = self.create_button(button_color, init_x - 120, baseline + 68 * self.HR, "<", font_color)
+        self.button_add = self.create_button(button_color, init_x + 40, baseline + 68 * self.HR, "+", font_color)
+        self.button_reset = self.create_button(button_color, init_x - 40, baseline + 68 * self.HR, "R", font_color,
+                                               48)
+        self.button_next = self.create_button(button_color, init_x + 120, baseline + 68 * self.HR, ">", font_color)
+
+        # index buttons
+        base_y = 264 * self.HR
+        button_color = "#7E6D4B"
+        self.button_index1 = self.create_button(button_color, 11 + 285 * 0 + 275 - 48, base_y + 352, "+", font_color, 48)
+        self.button_index2 = self.create_button(button_color, 11 + 285 * 1 + 275 - 48, base_y + 352, "+", font_color, 48)
+        self.button_index3 = self.create_button(button_color, 11 + 285 * 2 + 275 - 48, base_y + 352, "+", font_color, 48)
+        self.button_index4 = self.create_button(button_color, 11 + 285 * 3 + 275 - 48, base_y + 352, "+", font_color, 48)
+
+    def draw_menu_buttons(self):
         self.button_back.draw()
         self.button_next.draw()
         self.button_add.draw()
         self.button_reset.draw()
+
+    def draw_button_index(self, index):
+        if index == 1:
+            self.button_index1.draw()
+        elif index == 2:
+            self.button_index2.draw()
+        elif index == 3:
+            self.button_index3.draw()
+        elif index == 4:
+            self.button_index4.draw()
 
     def add_button_pressed(self, event):
         return self.button_add.is_clicked(event)
@@ -74,26 +95,60 @@ class SurfaceManager:
     def next_button_pressed(self, event):
         return self.button_next.is_clicked(event)
 
+    def index_button_pressed(self, event):
+        if self.button_index1.is_clicked(event):
+            return 1
+        elif self.button_index2.is_clicked(event):
+            return 2
+        elif self.button_index3.is_clicked(event):
+            return 3
+        elif self.button_index4.is_clicked(event):
+            return 4
+        else:
+            return 0
+
+    ##################
+    #     PANELS     #
+    ##################
+    def draw_background(self):
+        # bg color
+        self.draw_rect("#D9D9D9", (0, 0, self.WIDTH, self.HEIGHT))
+
+    def draw_progress_bar(self):
+        #progress bar
+        self.draw_rect("#FFFFFF", (136 * self.WR, 58 * self.HR, 1760 * self.WR, 180 * self.HR), 32)
+        self.draw_circle("#7E6D4B", 136 * self.WR, 148 * self.HR, 65)
+
+    def draw_mission_panel(self, card_count = 4, page = 1):
+        base_y = 264 * self.HR
+
+        #bg for reference
+        #self.draw_rect("#FF0000", (0, base_y, self.WIDTH, 680 * self.HR))
+
+        #display cards
+        for i in range(card_count):
+            card_color = "#F2EFEF"
+            card_index = 4 * (page - 1) + i
+            index_font = pg.font.SysFont("Arial", 32)
+
+            #Draw card and card text area
+            self.draw_rect(card_color, (11 + 285 * i, base_y + 8, 275, 392), border_radius=32)
+            self.draw_rect("#FFFFFF", (11 + 285 * i + 16, base_y + 8 + 16, 275 - 32, 392 - 104))
+
+            #Draw card buttons
+            self.draw_circle("#AAFFAA", 11 + 285 * i + 275 - 48, base_y + 400 - 48, 32)
+            self.draw_text(index_font, f"O", 11 + 285 * i + 275 - 58, base_y + 400 - 66)
+
+            self.draw_button_index(i+1)
+
     ##################
     #      MENU      #
     ##################
-    def init_buttons(self):
-        # temp buttons
-        baseline = self.HR * (264 + 680)
-        init_x = self.WIDTH / 2
-        button_color = "#EDEDED"
-        font_color = "#FFB435"
-
-        self.button_back = self.create_button(button_color, init_x - 120, baseline + 68 * self.HR, "<", font_color)
-        self.button_add = self.create_button(button_color, init_x + 40, baseline + 68 * self.HR, "+", font_color)
-        self.button_reset = self.create_button(button_color, init_x - 40, baseline + 68 * self.HR, "R", font_color, 48)
-        self.button_next = self.create_button(button_color, init_x + 120, baseline + 68 * self.HR, ">", font_color)
-
-    def draw_menu(self):
+    def draw_menu(self, card_count = 4, page = 1):
         self.draw_background()
         self.draw_progress_bar()
-        self.draw_mission_panel()
-        self.draw_buttons()
+        self.draw_mission_panel(card_count, page)
+        self.draw_menu_buttons()
 
 
 
